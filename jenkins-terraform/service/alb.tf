e598f9b7aa211c7aea01cbdc3945b9e11ccf56c7
NokkuPrudhvi2Main <49380061+NokkuPrudhvi@users.noreply.github.com>
2021-06-08 18:08:22 +0530

    Update alb.tf

--- jenkins-terraform/service/alb.tf
+++ jenkins-terraform/service/alb.tf
@@ -1,36 +1,47 @@
-resource "aws_lb" "cockroachdb" {
-  name               = format("%s-%s-%s-%s-nlb", var.prefix, var.region_name, var.stage, var.service)
-  internal           = false
-  load_balancer_type = "network"
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = format("%s-%s-%s-%s-alb", var.prefix, var.region_name, var.stage, var.service)
+
+  load_balancer_type = "application"
+
+  vpc_id             = data.terraform_remote_state.network.outputs.vpc_id
   subnets            = data.terraform_remote_state.network.outputs.public_subnets
+  security_groups    = ["sg-edcd9784", "sg-edcd9785"]
 
-  enable_deletion_protection = false
+  #access_logs = {
+  #  bucket = "my-alb-logs"
+  #}
 
-  tags = var.tags
-}
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 8080
+      target_type      = "instance"
+      targets = [
+        {
+          target_id = module.jenkins.id[0]
+          port = 8080
+        }
+      ]
+    }
+  ]
 
-resource "aws_lb_listener" "cockroachdb" {
-  load_balancer_arn = aws_lb.cockroachdb.arn
-  port              = "26257"
-  protocol          = "TCP"
+  https_listeners = [
+    {
+      port               = 443
+      protocol           = "HTTPS"
+      certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+      target_group_index = 0
+    }
+  ]
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.cockroachdb.arn
+  
+  tags = {
+    Environment = "Dev"
   }
 }
-
-resource "aws_lb_target_group" "cockroachdb" {
-  name     = format("%s-%s-%s-%s-tg", var.prefix, var.region_name, var.stage, var.service)
-  port     = 26257
-  protocol = "TCP"
-  vpc_id   = data.terraform_remote_state.network.outputs.vpc_id
-}
-
-resource "aws_lb_target_group_attachment" "cockroachdb" {
-  count      = var.aws_instances_count
-  target_group_arn = aws_lb_target_group.cockroachdb.arn
-  target_id        = module.cockroachdb.id[count.index]
-  port             = 26257
-}
-
+  
